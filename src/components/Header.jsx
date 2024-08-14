07fa7e458eeabf664a28779fa1cd724c69f268ff
Hugo Pereira <128017769+Hugo8814@users.noreply.github.com>
2024-08-14 23:31:45 +0100

    okay

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,20 @@
-import headerImg from "../img/header.jpg";
 function Header() {
   return (
     <div className="header">
-      <img className="header__img" src={headerImg} alt="" />
+      <div className="header__img">
+        <div className="header__Box">
+          <h1 className="header__Box-title">
+            Welcome to Welcome Massage Centre
+          </h1>
+          <h2 className="header__Box-subtitle">
+            Specialising in hotStone massage, EFT, Swedish Massage & Reflexology
+          </h2>
+          <div className="header__Btn">
+            <button className="header__Btn-BookNow">Book Now</button>
+            <button className="header__Box-Contact us">Treatments</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,14 +2,14 @@ import logoImg from "../img/logo.jpg";
 function Nav() {
   return (
     <nav className="nav">
-      <div className="nav__logo">
-        <img src={logoImg} />
+      <div className="nav__logoBox">
+        <img className="nav__logoBox-img" src={logoImg} />
       </div>
       <div className="nav__title">Welfare Massage</div>
-      <div className="nav__link">Welcome</div>
+      <div className="nav__link-pink">Welcome</div>
       <div className="nav__link">Treatments</div>
-      <div className="nav__link">revi</div>
-      <div className="nav__link">Welcome</div>
+      <div className="nav__link">Reviews</div>
+      <div className="nav__link">Contact</div>
     </nav>
   );
 }
