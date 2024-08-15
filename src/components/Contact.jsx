c2b96117961bfb4177f1a7e30ef0a75bf164a2c9
Hugo Pereira <128017769+Hugo8814@users.noreply.github.com>
2024-08-15 01:48:10 +0100

    okay

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,6 +3,20 @@ function Contact() {
     <div className="contact">
       <div className="contact__img">
         <div className="curved"></div>
+        <div className="contact__infoBox">
+          <div className="contact__OpenTimes">
+            <h3 className="contact__OpenTimes-title">Open Times</h3>
+            <p className="contact__OpenTimes-text">Monday: 09:30 - 19:00</p>
+            <p className="contact__OpenTimes-text">Tuesday: 09:30 - 19:00</p>
+            <p className="contact__OpenTimes-text">Wednesday: 09:30 - 19:00</p>
+            <p className="contact__OpenTimes-text">Thursday: 09:30 - 19:00</p>
+            <p className="contact__OpenTimes-text">Friday: 09:30 - 19:00</p>
+            <p className="contact__OpenTimes-text">Saturday: 09:30 - 19:00</p>
+            <p className="contact__OpenTimes-text">Sunday: Closed</p>
+          </div>
+          <div className="contact__line"></div>
+          <div className="contact__info"></div>
+        </div>
       </div>
     </div>
   );
