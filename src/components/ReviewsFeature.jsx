e52a58c3990a9aa1f2e9e73a32e2e35a47424cd0
Hugo Pereira <128017769+Hugo8814@users.noreply.github.com>
2024-08-16 22:52:28 +0100

    okay

--- src/components/ReviewsFeature.jsx
+++ src/components/ReviewsFeature.jsx
@@ -1,7 +1,15 @@
 function ReviewsFeature() {
   return (
-    <div className="reviewsFeature">
-      <div></div>
+    <div className="reviews__Feature">
+      <div className="reviews__card">
+        <div className="reviews__text">
+          <div className="reviews__text-titlebox">
+            <div className="reviews__text-logo">Google</div>
+            <div className="reviews__text-title">Reviews</div>
+          </div>
+        </div>
+        <button className="reviews__btn"> Review us on Google</button>
+      </div>
     </div>
   );
 }
