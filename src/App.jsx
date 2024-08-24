9f159952ef73b340eafe3f7725d7f11054feb4ff
Hugo Pereira <128017769+Hugo8814@users.noreply.github.com>
2024-08-24 22:27:32 +0100

    okay

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Treatments from "./components/Treatments";
 import Contact from "./components/Contact";
 import Location from "./components/Location";
 import ReviewsFeature from "./components/ReviewsFeature";
+import Bookbtn from "./components/Bookbtn";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Nav />
       <Header />
       <Treatments />
+      <Bookbtn />
       <Reviews />
       <ReviewsFeature />
       <Contact />
---
+++ src/components/Bookbtn.jsx
@@ -0,0 +1,12 @@
+function Bookbtn() {
+  return (
+    <div className="container">
+      <div className="header__Btn">
+        <button className="header__Btn-BookNow">Book Now</button>
+        <button className="header__Btn-Contact ">Contact us</button>
+      </div>
+    </div>
+  );
+}
+
+export default Bookbtn;
