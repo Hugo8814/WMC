d83514454ec023c4f2314c332ef644ce408e0ed5
Hugo Pereira <128017769+Hugo8814@users.noreply.github.com>
2024-08-28 16:44:15 +0100

    okay

--- src/components/Treatments.jsx
+++ src/components/Treatments.jsx
@@ -2,9 +2,12 @@ import img1 from "../img/img1.webp";
 import img2 from "../img/img2.webp";
 import img3 from "../img/img3.webp";
 //import img4 from "../img/img4.webp";
-import img5 from "../img/img5.webp";
+
 import img6 from "../img/img6.png";
-//new names
+import deepimg from "../img/deep_tissue.jpg";
+import sideimg from "../img/side.png";
+import footimg from "../img/foot.png";
+import bns from "../img/bns.png";
 
 function Treatments() {
   return (
@@ -16,11 +19,10 @@ function Treatments() {
           </div>
           <div className="grid__item-title">Swedish Massage</div>
           <div className="grid__item-description">
-            Swedish massage is a classic technique that relaxes the body and
-            mind. It uses long, smooth strokes, kneading, and gentle pressure to
-            relieve tension. This type of massage enhances circulation, eases
-            muscle pain, and promotes overall relaxation. Ideal for those new to
-            massage, it helps reduce stress and fosters a sense of well-being.
+            Swedish massage is a classic technique that relaxes body and mind.
+            Using long strokes, kneading, and light pressure, it relieves
+            tension, improves circulation, and reduces muscle pain. Ideal for
+            beginners, it promotes overall relaxation and well-being.
           </div>
         </div>
 
@@ -43,10 +45,10 @@ function Treatments() {
           <div className="grid__item-title">Lymphatic Drainage</div>
           <div className="grid__item-description">
             Lymphatic drainage massage stimulates the lymphatic system to
-            enhance fluid flow and reduce swelling. This gentle technique uses
-            light, rhythmic strokes to encourage the removal of toxins and
-            improve circulation. Ideal for reducing bloating and boosting
-            immunity, it promotes overall well-being and a sense of lightness.
+            improve fluid flow and reduce swelling. Using light, rhythmic
+            strokes, it removes toxins and boosts circulation. Ideal for
+            reducing bloating and boosting immunity, it promotes well-being and
+            a sense of lightness.
           </div>
         </div>
 
@@ -56,69 +58,64 @@ function Treatments() {
           </div>
           <div className="grid__item-title">Aromatherapy</div>
           <div className="grid__item-description">
-            Aromatherapy uses essential oils to enhance physical and emotional
-            well-being. By incorporating soothing scents into massage, it
-            promotes relaxation, reduces stress, and supports mental clarity.
-            Each essential oil offers unique benefits, providing a tailored
-            experience to uplift mood and rejuvenate the senses.
+            Aromatherapy uses essential oils to enhance well-being. Infusing
+            soothing scents into massage, it promotes relaxation, reduces
+            stress, and boosts mental clarity. Each oil offers unique benefits,
+            creating a tailored experience to uplift mood and refresh the
+            senses.
           </div>
         </div>
 
         <div className="grid__item">
           <div className="grid__item__imgBox">
-            <img className="grid__item-img" src={img3} />
+            <img className="grid__item-img" src={deepimg} />
           </div>
           <div className="grid__item-title">Deep Tissue</div>
           <div className="grid__item-description">
-            Deep tissue massage targets the deeper layers of muscles and
-            connective tissue. By applying firm pressure and slow strokes, it
-            helps alleviate chronic pain, reduce muscle tension, and improve
-            flexibility. This technique is ideal for addressing specific areas
-            of discomfort and promoting long-lasting relief.
+            Deep tissue massage targets deeper muscle layers and connective
+            tissue. With firm pressure and slow strokes, it relieves chronic
+            pain, reduces muscle tension, and improves flexibility. Ideal for
+            addressing specific discomfort, it promotes lasting relief.
           </div>
         </div>
 
         <div className="grid__item">
           <div className="grid__item__imgBox">
-            <img className="grid__item-img" src={img5} />
+            <img className="grid__item-img" src={sideimg} />
           </div>
-          <div className="grid__item-title">Sports Massage</div>
+          <div className="grid__item-title">Pregnacy massage</div>
           <div className="grid__item-description">
-            Sports massage focuses on preventing and treating injuries
-            associated with athletic activities. It combines various techniques
-            to enhance flexibility, reduce muscle soreness, and improve
-            performance. Ideal for athletes and active individuals, it aids in
-            faster recovery and maintains peak physical condition.
+            Pregnancy massage is a gentle treatment for pregnant women. Using
+            gentle techniques, it alleviates common discomforts such as back
+            pain and swelling, promotes relaxation, and improves circulation.
           </div>
         </div>
 
         <div className="grid__item">
           <div className="grid__item__imgBox">
-            <img className="grid__item-img" src={img6} />
+            <img className="grid__item-img" src={footimg} />
           </div>
           <div className="grid__item-title">Reflexology</div>
           <div className="grid__item-description">
-            Foot massage focuses on relieving tension and improving circulation
-            in the feet. Using gentle pressure and soothing techniques, it helps
-            to reduce stress, alleviate fatigue, and promote relaxation. Ideal
-            for soothing tired feet, this treatment revitalizes and refreshes,
-            offering a comforting escape from daily strain.
+            Reflexology relieves tension and improves circulation in the feet.
+            Using gentle pressure, it reduces stress, alleviates fatigue, and
+            promotes relaxation. Ideal for tired feet, it revitalizes and
+            refreshes, offering a soothing escape from daily strain.
           </div>
         </div>
         <div className="grid__item">
           <div className="grid__item__imgBox">
-            <img className="grid__item-img" src={img1} />
+            <img className="grid__item-img" src={bns} />
           </div>
           <div className="grid__item-title">
             Back, Neck & <br />
             Shoulders
           </div>
           <div className="grid__item-description">
-            A back, neck, and shoulders massage targets common areas of tension
-            and discomfort. This focused therapy uses techniques to relieve
-            muscle tightness, improve circulation, and alleviate stress. Ideal
-            for those experiencing stiffness or soreness, it promotes relaxation
-            and overall well-being.
+            A back, neck, and shoulders massage targets common tension areas. It
+            uses techniques to relieve muscle tightness, improve circulation,
+            and reduce stress. Ideal for stiffness or soreness, it promotes
+            relaxation and overall well-being.
           </div>
         </div>
       </div>
